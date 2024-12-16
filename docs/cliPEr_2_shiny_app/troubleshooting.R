ba48090596ece575a2387df57a9a4cb259eb19a9
calhoujd <45797058+calhoujd@users.noreply.github.com>
2024-12-16 11:41:49 -0600

    test

---
+++ docs/cliPEr_2_shiny_app/troubleshooting.R
@@ -0,0 +1,37 @@
+library(shiny)
+library(bslib)
+
+library(Biostrings)
+
+# troubleshooting
+
+# read in input file 1 ## this is the output from app 2 with enrichment/functional scores and SE
+## oh shoot this is e37 ### need to find/make an e37 dictionary
+df <- read.csv('/Users/jdc411/Downloads/data-2024-11-21.csv')
+
+# read in input file 2 ## this should be the dictionary file from cliPEpy_1 with variant names and k-mers
+df2 <- read.csv('/Users/jdc411/Downloads/TSC2_e37_kmer_dictionary.csv')
+
+
+# initiate empty list
+empty_list <- c()
+counter <- 1
+# convert to DNAstring to enable reverse complement function
+for (i in df$kmer) {
+  dna<-DNAString(x=i, start=1, nchar=NA)
+  rev<-reverseComplement(dna)
+  #print(dna)
+  #print(rev)
+  #print(as.character(rev))
+  empty_list[counter] <- as.character(rev)
+  counter = counter + 1
+}
+
+# works with no rev compl
+df$kmer <- toupper(df$kmer)
+merge_df <- merge(df,df2,by='kmer')
+
+df$kmer <- empty_list
+df$kmer <- toupper(df$kmer)
+merge_df <- merge(df,df2,by='kmer')
+
